1158b0976caa5c79f9728776e75778375092a8ca
Karen Caplan <keaslk@gmail.com>
2013-03-05 15:51:33 -0800

    initial commit for rails app Taskio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskio::Application.config.secret_token = '06a61704d58fbdf77e183cf75788af4c30f55ae2a3fc3c87630eb3295c201201df8547e1cc707ebca99d31856b8a09ba53c42c4b1ee2b6f8245ec9befdffbef6'
