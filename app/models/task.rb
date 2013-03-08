e38d1b8ecad3aafc705bf9594cf82f2313ff5dc1
Lauren <eastridgele@gmail.com>
2013-03-08 14:16:22 -0800

    add validations and errors for blank tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessible :created_at, :description, :priority, :title
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- spec/integration/creating_tasks_spec.rb
+++ spec/integration/creating_tasks_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 feature "Creating Tasks" do
-  scenario "create a task" do
+
+  before do
     visit '/tasks'
     click_link "New Task"
+  end
+
+  scenario "create a task" do
     fill_in "Title", with: "Clean the house"
-   # fill_in "Description", with: "Upstairs cleaning"
+    fill_in "Description", with: "Upstairs cleaning"
     # fill_in "Priority", :with => 2
     click_button "Create Task"
     page.should have_content("Your task has been created.")
   end
+
+  scenario "don't create an empty task" do
+  click_button "Create Task"
+  page.should have_content("Your task was not created.")
+  end
+
 end
