ebf9c391935ffb26c1dccedce492ce9a4e6f9c3c
Lauren <eastridgele@gmail.com>
2013-03-08 19:44:05 -0800

    add ancestry gem for nested resources. Migrate db

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
-  attr_accessible :created_at, :description, :priority, :title
+  attr_accessible :created_at, :description, :priority,
+                  :title, :parent_id
   validates :title, presence: true
   validates :description, presence: true
+  has_ancestry
 end
