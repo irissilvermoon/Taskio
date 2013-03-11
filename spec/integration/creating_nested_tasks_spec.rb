9fdb99af2d2ebdd75f35e97b2188123032b0eac3
Lauren <eastridgele@gmail.com>
2013-03-10 20:26:14 -0700

    nested tasks can be created

--- spec/integration/creating_nested_tasks_spec.rb
+++ spec/integration/creating_nested_tasks_spec.rb
@@ -5,15 +5,23 @@ feature "Creating Nested Tasks" do
   before do
     Factory(:task, title: "Clean the house")
     visit '/tasks'
-    click_link "Clean the house"
     click_link "Subtask"
   end
 
-  scenario "create a  nested task" do
+  scenario "create a nested task" do
     fill_in "Title", with: "Clean the bathroom"
     click_button "Create Task"
     page.should have_content("Your task has been created.")
   end
+
+  # scenario "nested tasks display on show" do
+  #   fill_in "Title", with: "Clean the kitchen"
+  #   click_button "Create Task"
+  #   visit '/tasks'
+  #   click_link "Clean the house"
+  #   page.should have_content("Clean the kitchen")
+  # end
+
 
   scenario "don't create an empty task" do
   click_button "Create Task"
